aef47db3a67eb0587b1c69fe9c35e84a271caeb0
William F Nicholson <williamnicholson99@gmail.com>
2021-10-31 22:06:05 -0400

    Added Wiki Content
    Check change log to see content added to wiki

--- js/dnd.js
+++ js/dnd.js
@@ -1,4 +1,4 @@
-var tree =
+let tree =
 [
     {
         text: "Worlds of Magnus",
@@ -115,6 +115,12 @@ var tree =
                                     {
                                         id:    "Lokali-People-Cultures-Kotal",
                                         text:  "Kotal",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Ngyemu",
+                                        text:  "Ngyemu",
                                         icon:  "fas fa-file",
                                         href:  ""
                                     },
@@ -238,3 +244,201 @@ var tree =
         icon: "fa fa-folder"
     }
 ];
+
+function loadAriales() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Ariales</h5>
+            <p class='card-text'>
+                West of Anes, southwest of the Kotal, lies the lands of Ariales where civilization is at the pinnacle of progress.
+                Houses of knowledge push the boundaries of knowledge and transfer it to the people of the city. It is here that the study of Magi begins,
+                and where all Magnus originate from. Deserts are plentiful here, as are the rivers providing bountiful harvests. 
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadAstis() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Astis</h5>
+            <p class='card-text'>
+                South of the Ariales lie the Astis, who live off rivers as much as the Ariales do, but instead of focusing on knowledge or military might,
+                the Astis are famed for their engineering marvels. Currently the Astis are vassilized by the Arilian Abdul Dynasty. 
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadCaybluff() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Caybluff</h5>
+            <p class='card-text'>
+                North of the Jansen and Vevide lie the island of Caybluff and its people. With a long history of conquest by the Cirilians,
+                Godobald, and most recently the Jansen, before gaining independence. Though they claim to be their own people,
+                the influence of other civilizations has changed the course of Caybluffian history. 
+                Originally a subculture of the northern Godobaldians, the Bluffians invaded the former Cirilian territory with survivors of the empire
+                and the local Caydian inhabitants. After four decades of fighting, the capital of Caydian was renamed to Caybluff to represent peace between
+                the two people. Marriage resulted in a rapid synthesis of the two cultures and a newly defined royal class.
+                After the conflict settled, boats were dispatched to Anes to explore the formerly Cirlian lands and create a ground for future expansion. 
+                Unable to claim the entire southern coast, the king of Upper Godobald brokered an agreement with the then queen of Caybluff to divide the land
+                and protect each other from foreign or domestic colonial threats.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadGara() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Gara</h5>
+            <p class='card-text'>
+                South of Astis is a thin portion of the crescent continent of Duria, where monsoons fuel the jungles of the region. The Gara are said to have
+                hailed from the planet of its same name, but their exact origins are questionable at best. Some things appear to be lost to history forever.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadGodobald() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Godobald</h5>
+            <p class='card-text'>
+                Mountains mostly separate the Tehueypac from the other civilizations of Durios. As the continent curves east, one can find the Godobald people.
+                A massive culture group containing people from differing climates and biomes, they are united through their stories and traditions of fighting the
+                Cirilians centuries ago. Having evolved technologically since such battles, the Godobald collectives are forming new governments and societies within
+                themselves. Region originated as tribal society united against Cirilian Empire invasion of south. Tribes last united by King Mariwig Adald, or Mariwig
+                the Noble. Vast Kingdom split by five sons who wared in the resulting 3 kingdoms and a derivative Vevide kingdom to the east (Upper & Lower Godobald, Gratus, Vevide).
+                Hardcore feudal society, barons, counts, dukes, kings, but no emperors… yet. Most of land congregated by dukes, only 1 kingdom prevented internal warfare in legal code
+                (King Vaefar of Upper Godobald). Upper Godobald wealthier of Godobald kingdoms given port and trade. o Negotiate cooperation pact with Caybluff to peacefully settle
+                former Cirilian lands and trade by splitting the continent in half, east (Godobald) and west (Caybluff). • Like most vegetation on Lokali, grains are thicker and denser
+                than on other worlds. o Unlike in other cultures however, Godobaldians do not attempt to thin out their food, and eat less of dense meals. 
+                Class system relies highly on warfare, low born are picked to inherit lands based on martial ability. Rebellions often result in local despots being 
+                overthrown by great warriors. • Desire to expand lands and exploit resources comes from trade with Arilians. To rival them in wealth, lands of Cirilian Empire
+                are explored, and land given to the best warriors.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadSkoro() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Skoro</h5>
+            <p class='card-text'>
+                Separated by a narrow sea in all parts but the most southern snowy plain, the Vevide are bordered by the Skoro raiders,
+                who thrive in the heat of battle. South of the Vevide and Skoro are small enclaves of Tizarnian people who are brave enough to be open
+                targets to the Skoro raiders. With a majority of the seasons being cold and not optimal for agriculture, grains are thinner than across the rest of the world,
+                and diets consist of more livestock, which can be sustained on the thinner grains of barely. • Originally stratified by a system of warrior classes, wealth
+                through sea travel raids brought change to the social structure of the land. Those who did not raid foreign shores attempted to raid local ones,
+                but a new system of tribal lords would defend the local shores while foreign raiders would plunder abroad. This created a divide between the loosely
+                organized tribes and those highly organized ones which exchanged food for protection. • Most loose tribes do not attempt to raid alone anymore and live
+                off the remote mountains on the eastern coast of the continent. These tribes are fleeting, every season more and more tribes people assimilate into the
+                highly organized tribes. • With some of the most sophisticated boats on the planet, the Skoro sail past Caybluff through the hard seas of the world,
+                onto both Anes and other continents throughout the world. • As local strife lessens, conflict turns towards raiding the world, fueling the local regional
+                wealth. Defended by mountains from the west, it is neigh impossible for war to conquer the Skoro through warfare. • The Cirilian Empire from Anes attempted
+                to wage war on the coasts of Skoro in eras past, leading to generations of hardened warriors to emerge. The Skoro learned from the Cirilian boat designs how
+                to cross the hard seas by deconstructing their technology and improving on it. The result was orchestrated raids on Cirilian lands and the start of the
+                raiding culture on Anes and the wider world.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadKotal() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Kotal</h5>
+            <p class='card-text'>
+                The Kotal reside to the far north of Anes on Lokali, a nomadic steppe people who rely on horses and other steppe animals to live their lives.
+                Living off the land, these people do not exploit the riches of the land that other civilizations might.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadNgyemu() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Ngyemu</h5>
+            <p class='card-text'>
+                Locked away from reaching the shores of Anes, the Ngyemu are congregation of tribal governments that trade goods across the desert to the
+                Arilians and Astis. Legends say that they possess mines of minerals that sparkle like the night sky.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadRyuga() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Ryuga</h5>
+            <p class='card-text'>
+                Southeast of the Kotal are the Ryugans, whose vast culture group ranges from the rivers of Tumen and the island of Ryuga itself.
+                Aside from skirmishes with the Kotal, these are a more peaceful people who only notion of war is war with oneself.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadTehueypec() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Tehueypec</h5>
+            <p class='card-text'>
+                Though the Gara hold a grip on the southern region of Duria and its surrounding islands, Durios, whose crescent curves south from west to east,
+                neighbors the Gara with a hostile civilization known as the Tehueypec. Said to have once inhabited the lands from the mountains on their southern
+                border to the jungles of southern Duria, they have a long standing gripe against the Gara who've fought for those lands for centuries. The Tehueypec
+                practice human sacrifice to the gods above.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadTizarna() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Tizarna</h5>
+            <p class='card-text'>
+                While Durios curves further east, to the south of the Godobald lie the Tizarna, who live in the tundra of the south, approaching the southern pole of the planet.
+            </p>
+        </div>
+    </div>
+    `
+}
+
+function loadVevide() {
+    return `
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>Vevide</h5>
+            <p class='card-text'>
+                As Durios curves no more and the continent meets its eastern end, the terrain becomes mountainous where the Vevide reside, next to the Godobald people.
+            </p>
+        </div>
+    </div>
+    `
+}
