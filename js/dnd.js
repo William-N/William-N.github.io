eb1c198988f472c265fffd6119bdcd82efcfc88a
William F Nicholson <williamnicholson99@gmail.com>
2021-10-31 20:08:17 -0400

    Major Changes
    Changed file structure
    Implemented wiki page structure
    Added content pages

---
+++ js/dnd.js
@@ -0,0 +1,240 @@
+var tree =
+[
+    {
+        text: "Worlds of Magnus",
+        icon: "fa fa-folder",
+        nodes:
+        [
+            {
+                text: "Ciri",
+                icon: "fa fa-folder",
+            },
+            {
+                text: "Wolin",
+                icon: "fa fa-folder"
+            },
+            {
+                text: "Lokali",
+                icon: "fa fa-folder",
+                nodes:
+                [
+                    {
+                        text:  "Geography",
+                        icon:  "fa fa-folder",
+                        nodes:
+                        [
+                            {
+                                text: "Climate",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Continents",
+                                icon: "fa fa-folder",
+                                nodes:
+                                [
+                                    {
+                                        id:    "Lokali-Geography-Continents-Anes",
+                                        text:  "Anes",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-Geography-Continents-Durios",
+                                        text:  "Durios",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-Geography-Continents-Durios",
+                                        text:  "Durios",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    }
+                                ]
+                            },
+                            {
+                                text: "Towns & Cities",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Rivers",
+                                icon: "fa fa-folder"
+                            }
+                        ]
+                    },
+                    {
+                        text: "People",
+                        icon: "fa fa-folder",
+                        nodes:
+                        [
+                            {
+                                text: "Races",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Cultures",
+                                icon: "fa fa-folder",
+                                nodes:
+                                [
+                                    {
+                                        id:    "Lokali-People-Cultures-Ariales",
+                                        text:  "Ariales",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Astis",
+                                        text:  "Astis",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Caybluff",
+                                        text:  "Caybluff",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Gara",
+                                        text:  "Gara",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Godobald",
+                                        text:  "Godobald",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Skoro",
+                                        text:  "Skoro",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Kotal",
+                                        text:  "Kotal",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Ryuga",
+                                        text:  "Ryuga",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Tehueypec",
+                                        text:  "Tehueypec",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Tizarna",
+                                        text:  "Tizarna",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Cultures-Vevide",
+                                        text:  "Vevide",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    }
+                                ]
+                            },
+                            {
+                                text: "Technology",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Government",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Economics",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Society",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Religion",
+                                icon: "fa fa-folder",
+                                nodes:
+                                [
+                                    {
+                                        id:    "Lokali-People-Religion-Skorian-Pantheon",
+                                        text:  "Skorian Pantheon",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Religion-Ulah",
+                                        text:  "Ulah",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Religion-Ryugan-Philosophy",
+                                        text:  "Ryugan Philosophy",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Religion-Wastrada",
+                                        text:  "Wastrada",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Religion-Ciri-Wolin",
+                                        text:  "Ciri & Wolin",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    },
+                                    {
+                                        id:    "Lokali-People-Religion-Dhinir-Duohr",
+                                        text:  "Dhinir & Duohr",
+                                        icon:  "fas fa-file",
+                                        href:  ""
+                                    }
+                                ]
+                            },
+                            {
+                                text: "History",
+                                icon: "fa fa-folder"
+                            },
+                            {
+                                text: "Language",
+                                icon: "fa fa-folder"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                text: "Osada",
+                icon: "fa fa-folder"
+            },
+            {
+                text: "Gara",
+                icon: "fa fa-folder"
+            }
+        ]
+    },
+    {
+        text: "Beyond Magnus",
+        icon: "fa fa-folder"
+    },
+    {
+        text: "Magi",
+        icon: "fa fa-folder"
+    },
+    {
+        text: "Game Master Materials",
+        icon: "fa fa-folder"
+    }
+];
