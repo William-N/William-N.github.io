fadfcbd88e999d62a23146cbe4e89635613c6901
William F Nicholson <williamnicholson99@gmail.com>
2021-11-06 14:43:22 -0400

    Updated Search Feature
    Uses copy of elements instead of direct copies. Next steps: use jQuery to ensure events are bound to then copied elements and to enable more control over the page.

--- js/dnd-search.js
+++ js/dnd-search.js
@@ -1,12 +1,17 @@
 function searchTree() {
-    // Declare variables
-    let input = document.getElementById("dnd-search-input")
-    let wikiPageValues = document.querySelectorAll("div.list-group-item")
+    const input = document.getElementById("dnd-search-input")
+    const searchResultsElement = document.getElementById("search-results")
+    const wikiPageValues = document.querySelectorAll("div.list-group-item")
     let searchValue = input.value.toUpperCase()
-    let searchResultsElement = document.getElementById("search-results")
     let displayValues = []
 
-    searchResultsElement.innerHTML = ""
+    // clear search-results DOM element when it is populated
+    if(searchResultsElement.innerHTML) {
+        while(searchResultsElement.childElementCount > 0) {
+            let child = searchResultsElement.childNodes.item(0)
+            searchResultsElement.removeChild(child)
+        }
+    }
 
     // if search value is empty, terminate
     if(!searchValue) {
@@ -17,19 +22,21 @@ function searchTree() {
         // Each folderOrFile has a singular 'i' tag
         folderOrFile.childNodes.forEach(iTag => {
             // This is a 'file'
-            console.log(iTag)
             if(iTag.className === "item-icon fas fa-file") {
                 // Proceed if the file's name is in the search value
                 if(folderOrFile.innerText.toUpperCase().indexOf(searchValue) > -1) {
+                    // Create copy of DOM element
+                    let newFile = folderOrFile.cloneNode(true)
                     // Add file to array that'll populate HTML
-                    displayValues.push(folderOrFile)
+                    newFile.removeAttribute("style")
+                    displayValues.push(newFile)
                 } 
             }
         })
     })
 
     // Add values to the search results HTML
-    displayValues.forEach(elem => {
-        searchResultsElement.appendChild(elem)
+    displayValues.forEach(folder => {
+        searchResultsElement.appendChild(folder)
     })
 }
